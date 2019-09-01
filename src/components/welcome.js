c0097a80ae5bad01523e3cd47bfbf541bdb1cc83
ahatcher8 <drewhatcher@gmail.com>
2019-09-01 18:56:27 -0400

    Testing different buttons to link to Home

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Image from 'react-bootstrap/Image'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 
 function Welcome() {
@@ -23,17 +23,25 @@ function Welcome() {
                 />
                 {"Drew's Site"}
             </Navbar.Brand>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
-            </Form>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
             </Navbar>
         </div>
         <div class="container">
             <Image src={require("../assets/NoahSunburst.jpg")} className="bg"/>
             <div class="centered">
                 <p>I am vertically and horizontally centered. </p>
-                <Button onClick="goHome();" variant="outline-light" size="sm" >Enter</Button>
+                <Button onClick={() => { goHome() }} variant="outline-light" size="sm" >Enter</Button>
+                <Button eventKey={1} componentClass={Link} href="/" to="components/Home" variant="outline-light" size="lg" >Enter2</Button>
+                <Button onClick="location.href='http://www.google.com'" type="button" variant="outline-light" >Enter3</Button>
+                <Link to="/components/Home">
+                    <Button renderAs="button">
+                        <span>Enter4</span>
+                    </Button>
+                </Link>
+                
             </div>
         </div>
       </>
@@ -44,11 +52,11 @@ function goHome() {
     return (
         <>
             <Router>
-                <div>
-                    <Route exact path="/" component={Home} />
-                </div>
+            <div>
+                <Route exact component={Home} />
+            </div>
             </Router>
-        </>
+       </>
     )
 }
 
